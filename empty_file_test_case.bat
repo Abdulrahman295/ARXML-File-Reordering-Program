975d1d5e29a2953fb49bbbbea6bc6ebea7e98008
Abdulrahman Ibrahim <89452130+Abdulrahman295@users.noreply.github.com>
2023-04-19 14:23:36 +0200

    Delete empty_file_test_case.bat

--- empty_file_test_case.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set CLASSPATH=bin;lib\*;src
-javac -d bin -cp %CLASSPATH% src\*.java src\AnnotatedClasses\*.java src\UserDefinedException\*.java
-java -cp %CLASSPATH% App empty.arxml
-pause
